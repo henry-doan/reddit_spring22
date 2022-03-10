9944bb43fdcee0bd7b863916bc9c49f866cffa8b
Henry Doan <henrydoan96@gmail.com>
2022-03-09 18:46:15 -0700

    added react router dom

--- client/src/components/topics/Topics.js
+++ client/src/components/topics/Topics.js
@@ -2,9 +2,17 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import TopicForm from './TopicForm';
 import TopicList from './TopicList';
+import { useLocation, useParams } from 'react-router-dom';
 
-const Topics = ({ subId }) => {
+// const Topics = ({ subId }) => {
+const Topics = ({}) => {
   const [topics, setTopics] = useState([])
+  // if you want to grab the id from the link state then:
+  const location = useLocation();
+  const { subId, subTitle } = location.state
+
+  // is you want to grab the id from the url then
+  // const { subId,  } = useParams()
 
   useEffect( () => {
     axios.get(`/api/subs/${subId}/topics`)
@@ -25,6 +33,7 @@ const Topics = ({ subId }) => {
 
   return (
     <>
+      <h1>Sub: {subTitle}</h1>
       <h3>Topics</h3>
       <TopicForm addTopic={addTopic} />
       <TopicList
